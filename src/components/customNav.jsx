6687d7ea3ae4a255b1a3903e23d7f467c440ae63
Dwayne <driv91197@gmail.com>
2020-05-17 19:52:28 -0400

    Added content to Navbar. Added needed code

--- src/components/customNav.jsx
+++ src/components/customNav.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './customNav.css';
 
-export default class Navbar extends Component {
+export default class customNav extends Component {
     render() {
         return (
-            <Navbar>
-                
+            <Navbar collapseOnSelect expand='md'>
+                <Navbar.Brand className='mx-auto'>Dwayne Rivera</Navbar.Brand>
+                <Navbar.Toggle />
+
+                <Navbar.Collapse>
+                    <Nav className="ml-auto">
+                        <Nav.Link eventKey={1} to='/'>ABOUT</Nav.Link>
+                        <Nav.Link eventKey={2} to='/'>RESUME</Nav.Link>
+                        <Nav.Link eventKey={3} to='/'>CONTACT</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         )
     }
