8ee514c4453acfbeaef58b2ed1317faef1876229
Dwayne <driv91197@gmail.com>
2020-05-17 18:42:42 -0400

    inits, base code

---
+++ src/components/customNav.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <Navbar>
+                
+            </Navbar>
+        )
+    }
+}
---
+++ src/components/home.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <h1>Home page</h1>
+        )
+    }
+}
