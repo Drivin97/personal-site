9a70bf9b3ac2e217abf8917ae17d7f28739e0822
Dwayne <driv91197@gmail.com>
2020-05-19 22:27:20 -0400

    Nothing serious

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/customNav';
-import Home from './components/home'
+import Home from './components/home';
 
 class App extends Component {
   render() {
