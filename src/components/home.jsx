fd2a450d146252d9172735957e360f86a85e8c91
Dwayne <driv91197@gmail.com>
2020-05-21 01:12:34 -0400

    Home element updated. Added jumbotron

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import Image from 'react-bootstrap/Image';
-import ListGroup from 'react-bootstrap/ListGroup'
-import './home.css'
+//import ListGroup from 'react-bootstrap/ListGroup';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import './home.css';
 
 export default class Home extends Component {
     render() {
         return (
             <div className='home'>
-                <Image src='./assets/itsme.png' fluid></Image>
-                <ListGroup variant="flush">
+                <Image src='./assets/itsme.png'></Image>
+                {/* <ListGroup variant="flush">
                     <ListGroup.Item>New Paltz Alumn</ListGroup.Item>
                     <ListGroup.Item>Aspiring Software Engineer</ListGroup.Item>
                     <ListGroup.Item>Determined</ListGroup.Item>
                     <ListGroup.Item>Passionate</ListGroup.Item>
-                </ListGroup>
+                </ListGroup> */}
+            <Jumbotron>
+                <h1 className='hi'>Hi, I'm Dwayne Rivera</h1>
+                <p className='based'>An aspiring software engineer based in New York, NY</p>
+            </Jumbotron>
             </div>
         )
     };
