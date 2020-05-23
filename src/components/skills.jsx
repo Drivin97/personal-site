03e9a1b832856b2ad4f91762a56239a9d5e11908
Dwayne <driv91197@gmail.com>
2020-05-22 22:50:06 -0400

    Creation of projects and skills page

---
+++ src/components/skills.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './skills.css';
+
+export default class Skills extends Component {
+    render() {
+        return (
+            <div className='skills'>
+
+            </div>
+        )
+    };
+}
