6ab63531b7cb133f61de4a81c476364a453f4457
Dwayne <driv91197@gmail.com>
2020-05-25 18:28:29 -0400

    Changed logos, added logos to site.

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
 import './skills.css';
 
 export default class Skills extends Component {
@@ -6,6 +8,11 @@ export default class Skills extends Component {
         return (
             <div className='skills'>
                 <h1 className='page-name'>Skills</h1>
+                    <Container>
+                        <Image src='./assets/java-logo.jpg' />
+                        <Image src='./assets/react-logo.jpg' />
+                        <Image src='./assets/web-logo.jpg' />
+                    </Container>
             </div>
         )
     };
