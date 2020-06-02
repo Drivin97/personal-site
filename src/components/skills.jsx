f72b04e24b82b13cc1fa3b15378ad809fa2b13fc
Dwayne <driv91197@gmail.com>
2020-06-02 11:40:34 -0400

    Added 'learning' section to skills

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -8,11 +8,17 @@ export default class Skills extends Component {
         return (
             <div className='skills' id='skills-page'>
                 <h1 className='page-name'>Skills</h1>
-                    <Container>
-                        <Image src='./assets/java-logo.jpg' />
-                        <Image src='./assets/react-logo.jpg' />
-                        <Image src='./assets/web-logo.jpg' />
-                    </Container>
+                <Container>
+                    <Image src='./assets/java-logo.jpg' />
+                    <Image src='./assets/react-logo.jpg' />
+                    <Image src='./assets/web-logo.jpg' />
+                </Container>
+                <h1 className='page-name'>Learning</h1>
+                <Container>
+                    <Image src='./assets/PHP-Logo.jpg' />
+                    <Image src='./assets/sql-logo.jpg' />
+                    <Image src='./assets/Python-Logo.jpg' />
+                </Container>
             </div>
         )
     };
